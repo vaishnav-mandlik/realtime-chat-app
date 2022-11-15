afdba1767c80284db754f04918888c65631b10cf
Vaishnav Mandlik <vaishnavmandlik@yahoo.com>
2022-11-15 18:35:06 +0530

    sidebar

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import Chats from "./Chats";
+import Navbar from "./Navbar";
+import Search from "./Search";
+
+const Sidebar = () => {
+  return (
+    <>
+      <Box flex={1} border="1px">
+        <Navbar></Navbar>
+        <Search />
+        <Chats />
+      </Box>
+    </>
+  );
+};
+
+export default Sidebar;
