40bc489c7274f11ab19818e46bfe425251eb48b0
vaishanv-mandlik <vaishnavmandlik@yahoo.com>
2022-11-15 18:35:06 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Chat from "./Pages/Chat";
+import Login from "./Pages/Login";
 import SignUp from "./Pages/SignUp";
 
 function App() {
-  return <SignUp></SignUp>;
+  return <Chat></Chat>;
 }
 
 export default App;
